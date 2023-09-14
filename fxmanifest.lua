49306988ce93484f76dd36ab189262cf7e4e552f
Brown Development <130308145+Brown-Development@users.noreply.github.com>
2023-09-14 05:29:39 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+author 'Brown Development'
+description 'Housing Records for Quasar Housing'
+game 'gta5'
+lua54 'yes'
+
+client_scripts{
+    'code/client.lua'
+}
+
+server_scripts{
+    '@oxmysql/lib/MySQL.lua',
+    'code/server.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
